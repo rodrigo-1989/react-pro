b1b3201bbae0b820efa6a3654ac4283378508237
Julio Rodrigo <julio.lopez@oreillyauto.com>
2024-08-09 12:41:28 -0600

    Component extends styles

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,26 +1,29 @@
-import { createContext, useContext } from 'react';
+import { createContext, CSSProperties, ReactElement } from 'react';
 import { useProduct } from '../hooks/useProduct'
 import styles from '../styles/styles.module.css'
 
-import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces';
+import { ProductContextProps, Product } from '../interfaces/interfaces';
 
+export interface Props {
+  product: Product;
+  children?: ReactElement | ReactElement[];
+  className?: string;
+  style?: CSSProperties
+}
 
 export const ProductContext = createContext({} as ProductContextProps);
 
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ product, children }: ProductCardProps) => {
+export const ProductCard = ({ product, children, className, style }: Props) => {
 
   const { counter, increaseBy } = useProduct();
 
   return (
     <Provider value={{ counter, increaseBy, product }}>
 
-      <div className={styles.productCard}>
+      <div className={`${styles.productCard} ${className} `} style={style} >
         {children}
-        {/* <ProductImage img={product.img} />
-        <ProductTitle title={product.title} />
-        <ProductButtons /> */}
       </div>
     </Provider>
   )
